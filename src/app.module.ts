4cf9c7a83895abd7127fe32adb2d885c51936d7e
Oleh Lozovyk <oleh.lozovyk@gmail.com>
2026-07-25 15:12:00 +0300

    feat: setup TypeORM with Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppConfigModule } from './app-config/app-config.module';
+import { AppConfigService } from './app-config/app-config.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { entities } from './database/data-source';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [
+    AppConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [AppConfigModule],
+      useFactory: (config: AppConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: config.db.user,
+        password: config.db.password,
+        database: config.db.name,
+        ...entities,
+      }),
+      inject: [AppConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
