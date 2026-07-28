d236179fd7ab0583120d0e5ab12302952cd38b15
Oleh Lozovyk <oleh.lozovyk@gmail.com>
2026-07-28 19:30:48 +0300

    fix: specify column types for nullable union fields

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource, type DataSourceOptions } from 'typeorm';
+
+import { User } from '../user/user.entity';
 
 dotenv.config();
 
 export const entities: Pick<DataSourceOptions, 'entities'> = {
-  entities: [],
+  entities: [User],
 };
 
 const DB_PORT = process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432;
@@ -13,14 +15,15 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
-export const dataSource = new DataSource({
+const dataSource = new DataSource({
   type: 'postgres',
   host: DB_HOST,
   port: DB_PORT,
   username: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
-  synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.js,.ts}'],
   ...entities,
 });
+
+export default dataSource;
