e957c96f65eee6f01c99ed73e375d33c27b32fbe
Oleh Lozovyk <oleh.lozovyk@gmail.com>
2026-08-01 19:12:39 +0300

    feat: add CreateUserEntity migration

---
+++ src/database/migrations/1785593292762-CreateUserEntity.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateUserEntity1785593292762 implements MigrationInterface {
+  name = 'CreateUserEntity1785593292762';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying(255) NOT NULL, "password" character varying NOT NULL, "first_name" character varying(100), "last_name" character varying(100), "user_role" "public"."users_user_role_enum" NOT NULL DEFAULT 'user', "email_verified" boolean NOT NULL DEFAULT false, "email_verification_token" character varying, "email_verification_expires" TIMESTAMP WITH TIME ZONE, "avatar_url" character varying, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "users"`);
+  }
+}
